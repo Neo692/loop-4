9a1065ba005761c388b2e0d38e6989e3b1340489
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-17 13:49:07 +0100

    Add project files.

---
+++ loop 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace upg4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string password;
+
+        do
+        {
+            Console.Write("Ange lösenord: ");
+            password = Console.ReadLine();
+        }
+        while (password != "hemligt");
+
+        Console.WriteLine("Rätt lösenord. Du är inloggad.");
+    }
+}
